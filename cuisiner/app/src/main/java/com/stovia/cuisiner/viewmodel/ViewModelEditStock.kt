88e76f9870fb1cb32e086ba343bf5fdac03f75bb
juandurefaccini <juanchidure@outlook.com>
2021-01-20 09:27:31 -0300

    delete default string

--- cuisiner/app/src/main/java/com/stovia/cuisiner/viewmodel/ViewModelEditStock.kt
+++ cuisiner/app/src/main/java/com/stovia/cuisiner/viewmodel/ViewModelEditStock.kt
@@ -7,7 +7,7 @@ import java.util.*
 class ViewModelEditStock : ViewModel(){
     private val manageStockUseCase = ManageStockUseCase()
 
-    fun saveData(email:String,productName: String = "", productAmount: String, productUnit: String) {
+    fun saveData(email:String,productName: String, productAmount: String, productUnit: String) {
         if(productName.isNotBlank() && productAmount.isNotBlank() && productUnit.isNotBlank()){
             manageStockUseCase.updateProduct(email,
                 productName.toLowerCase(Locale.ROOT),
