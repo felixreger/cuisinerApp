5232c5c0a4ee98b69efd207736443a14da6e2e6d
juandurefaccini <juanchidure@outlook.com>
2021-01-20 12:13:51 -0300

    Actualizados los xml y agregados los vectorizados

--- cuisiner/app/src/main/java/com/stovia/cuisiner/ui/MainActivity.kt
+++ cuisiner/app/src/main/java/com/stovia/cuisiner/ui/MainActivity.kt
@@ -2,7 +2,13 @@ package com.stovia.cuisiner.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stovia.cuisiner.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 //navhost
